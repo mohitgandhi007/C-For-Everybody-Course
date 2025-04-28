94ef674f6935672452d0820b0f3771f893221a9b
Mohit Gandhi <gandhimohit.study@gmail.com>
2025-04-28 17:45:19 +0530

    Add files via upload

---
+++ assignments/poem.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Jhony Jhony, Yes papa!\n"
+           "Eating sugar?, No papa!\n"
+           "Telling lies?, No papa!\n"
+           "Open your mouth, Ha! Ha! Ha!");
+    return 0;
+}
